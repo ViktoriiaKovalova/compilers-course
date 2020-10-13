d0ece362276d4d4778e6b37500ad8cea518eef54
Viktoriia Kovalova <vikka.kovalova@gmail.com>
2020-10-13 18:53:10 +0300

    Add rex2lts and lts accepts method

---
+++ lts.py
@@ -0,0 +1,84 @@
+import typing as tp
+from collections import defaultdict
+from dataclasses import dataclass
+
+
+@dataclass(frozen=True)
+class Transition:
+    """
+    Transition represents the transition in LTS
+    and contains two states and label of transition.
+    """
+    from_: int
+    lbl: str
+    to: int
+
+
+class LTS:
+    """
+    LTS represents the labelled transition system.
+    Constructor parameters are starting state, ending state, set of states, tokens and transitions.
+    """
+
+    def __init__(self, start: int, end: int, states: tp.Set[int], tokens: tp.Set[str], transitions: tp.Set[Transition]):
+        self.start = start
+        self.end = end
+        self.tokens = tokens
+        self.states = states
+        self.transitions = transitions
+        # we store transitions in dict with keys (from, lbl) to get faster access to transitions
+        # given state, from which transitions are needed, and label
+        trans_from_with_label: tp.Dict[tp.Tuple[int, str], tp.List[Transition]] = defaultdict(list)
+        for tr in transitions:
+            trans_from_with_label[(tr.from_, tr.lbl)].append(tr)
+        self.trans_from_lbl = dict(trans_from_with_label)
+
+    def __repr__(self) -> str:
+        return f"start: {self.start}, end: {self.end}, transitions: {sorted(self.transitions, key=lambda x: x.from_)}"
+
+    def get_start(self) -> int:
+        return self.start
+
+    def get_end(self) -> int:
+        return self.end
+
+    def get_states(self) -> tp.Set[int]:
+        return self.states
+
+    def get_tokens(self) -> tp.Set[str]:
+        return self.tokens
+
+    def get_transitions(self) -> tp.Set[Transition]:
+        return self.transitions
+
+    def closure(self, state: int) -> tp.Set[int]:
+        """
+        returns the closure of given state, e.g. all states reachable via epsilon-transitions
+        """
+        set_ = {state}
+        added = [state]
+        while added:
+            cur = added.pop()
+            # if no epsilon transitions
+            if self.trans_from_lbl.get((cur, "")) is None:
+                continue
+            for tr in self.trans_from_lbl[(cur, "")]:
+                if tr.to not in set_:
+                    set_.add(tr.to)
+                    added.append(tr.to)
+        return set_
+
+    def accepts(self, chain: tp.Sequence[str]) -> bool:
+        reached = {(x, 0) for x in self.closure(self.start)}
+        while reached:
+            state, length = reached.pop()
+            # if we found good path
+            if length == len(chain) and state == self.end:
+                return True
+            # if path is longer than chain
+            if length >= len(chain):
+                continue
+            if self.trans_from_lbl.get((state, chain[length])):
+                for tr in self.trans_from_lbl[(state, chain[length])]:
+                    reached.update([(x, length + 1) for x in self.closure(tr.to)])
+        return False
---
+++ rex2lts.py
@@ -0,0 +1,53 @@
+from lts import LTS, Transition
+from rex import ReX, Epsilon, Symbol, KleeneStar, Union, Concatenation
+import typing as tp
+
+
+def rex2lts(rexp: ReX, first_state=0) -> LTS:
+    """
+    Builds LTS from given regexp.
+    States will be numbered as 0, ..., len(states) - 1.
+    First one is starting state, last one - ending state.
+    First_state is used to avoid collisions of states in recursive calls.
+    """
+    if isinstance(rexp, Epsilon) or isinstance(rexp, Symbol):
+        label = ""
+        if isinstance(rexp, Symbol):
+            label = rexp.value
+        start, end = first_state, first_state + 1
+        transitions = {Transition(start, label, end)}
+    elif isinstance(rexp, KleeneStar):
+        lts_inner = rex2lts(rexp.inner, first_state + 1)  # we reserve first state for start
+        start, end = first_state, lts_inner.end + 1
+        transitions = lts_inner.transitions
+        transitions.add(Transition(start, "", lts_inner.start))
+        transitions.add(Transition(lts_inner.end, "", end))
+        transitions.add(Transition(lts_inner.end, "", lts_inner.start))
+        transitions.add(Transition(lts_inner.start, "", lts_inner.end))
+    elif isinstance(rexp, Union):
+        lts_fir = rex2lts(rexp.first, first_state + 1)  # first state is for start
+        lts_sec = rex2lts(rexp.second, first_state + 1 + len(lts_fir.states))  # states from first lts are used already
+        start, end = first_state, lts_sec.end + 1
+        transitions = lts_fir.transitions
+        transitions.update(lts_sec.transitions)
+        transitions.add(Transition(start, "", lts_fir.start))
+        transitions.add(Transition(start, "", lts_sec.start))
+        transitions.add(Transition(lts_fir.end, "", end))
+        transitions.add(Transition(lts_sec.end, "", end))
+    elif isinstance(rexp, Concatenation):
+        lts_fir = rex2lts(rexp.first)
+        lts_sec = rex2lts(rexp.second, first_state + len(lts_fir.states))  # states from first lts are used already
+        start, end = lts_fir.start, lts_sec.end
+        transitions = lts_fir.transitions
+        transitions.update(lts_sec.transitions)
+        transitions.add(Transition(lts_fir.end, "", lts_sec.start))
+    else:
+        raise TypeError("Unknown regexp")
+    return LTS(start, end, range(start, end + 1), {x.lbl for x in transitions}, transitions)
+
+
+# examples
+a_lts = rex2lts(Symbol('a'))
+print(a_lts)  # start: 0, end: 1, transitions: {Transition(from_=0, lbl='a', to=1)}
+print(a_lts.accepts("a"))  # True
+print(a_lts.accepts("b"))  # False
